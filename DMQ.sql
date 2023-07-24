8c1f4e5fe4f33c3133849267a7ac5a97577c9734
Kevin <electrichexagon8@gmail.com>
2023-07-24 16:45:21 -0700

    Update DMQ.sql

--- DMQ.sql
+++ DMQ.sql
@@ -2,15 +2,23 @@
 
 -- ===== Customers =====
 
--- Get all customers
-SELECT name, username, email FROM Customers;
+-- SELECT all customer information
+SELECT * FROM Customers;
 
--- Add a new customer
+-- INSERT a new customer
 INSERT INTO Customers (name, username, email) VALUES
 (:nameInput, :usernameInput, :emailInput);
 
+-- UPDATE a customer's information
+UPDATE Customers
+SET name = :nameInput, username = :usernameInput, email = :emailInput
+WHERE customerID = :customerIDInput;
+
 
 -- ===== Orders =====
+
+-- SELECT all order information
+SELECT * FROM Orders;
 
 -- Get all orders with game names and customer names grouped together by customer name
 SELECT Customers.name, Games.title, Orders.orderDate, Orders.pricePaid
@@ -20,15 +28,35 @@ INNER JOIN Games ON Games.gameID = Orders.gameID
 GROUP BY Customers.name
 ORDER BY Customers.name DESC;
 
--- Place a new order
+-- INSERT/Place a new order
 INSERT INTO Orders (customerID, gameID, orderDate, pricePaid) VALUES
 (:customerIDInput, :gameIDInput, GETDATE(), :pricePaidInput);
 
 
 -- ===== Studios =====
 
+-- SELECT all studio information
+SELECT * FROM Studios;
+
+-- INSERT/Create a new game studio
+INSERT INTO Studios (name) VALUES
+(;nameInput);
 
 -- ===== Games =====
+
+-- SELECT all game information
+SELECT * FROM Games;
+
+-- INSERT/Add a new game
+INSERT INTO Games (studioID, title, publishDate, price) VALUES
+(:studioIDInput, :titleInput, :publishDateInput, :priceInput);
+
+-- DELETE a game
+DELETE FROM Games WHERE gameID=:gameIDInput;
+
+-- SEARCH for a game based on title
+SELECT * FROM Games
+WHERE CONTAINS(title, :titleInput);
 
 
 -- ===== Games Genres =====
@@ -36,4 +64,10 @@ INSERT INTO Orders (customerID, gameID, orderDate, pricePaid) VALUES
 
 -- ===== Genres =====
 
+-- SELECT/List all genres
+SELECT * From Genres;
+
+-- INSERT a new genre
+INSERT INTO Genres (name) VALUES
+(:nameInput);
 
