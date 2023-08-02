c98d621d2709977cbbff44d467eebdb5b0ce7902
Colton Melhase <melhasec@flip3.engr.oregonstate.edu>
2023-08-01 21:56:15 -0700

    Add setup

--- app.js
+++ app.js
@@ -4,8 +4,19 @@
     SETUP
 */
 var express = require('express');   // We are using the express library for the web server
-var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 3211;                 // Set a port number at the top so it's easy to change in the future
+var app     = express();           // We need to instantiate an express object to interact with the server in our code
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+PORT        = 22221;                 // Set a port number at the top so it's easy to change in the future
+
+var db = require('./database/db-connector');
+
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');     // Import express-handlebars
+app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+
 
 /*
     ROUTES
