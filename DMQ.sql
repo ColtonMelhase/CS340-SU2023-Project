da006c2a30aa0809cd81d11975a52d5d216641aa
Colton Melhase <coltonmelhase12@gmail.com>
2023-08-14 14:53:21 -0700

    Update, organize, and comment data manipulation queries

--- DMQ.sql
+++ DMQ.sql
@@ -1,72 +1,167 @@
--- Data manipulation queries
+-- Colton Melhase
+-- Kevin Rossel
+-- CS372
+-- Data Manipulation Queries
 
+-- *NOTE: ALL QUERY VARIABLES ARE DENOTED BY THE ${} TEMPLATE LITERAL.
+
+-- #####################################################################
+-- ===== Universal Queries =====
+-- #####################################################################
+
+-- This query is within the universal route in app.js
+-- It is a more flexible route where it can take both the table and id to return
+-- the specific tuple. This prevents having one route per table when this one route can
+-- return the same result.
+Select * 
+FROM ${table} 
+WHERE ${attr}ID=${req.params.id};
+
+-- #####################################################################
 -- ===== Customers =====
+-- #####################################################################
 
--- SELECT all customer information
-SELECT * FROM Customers;
+-- This query is used when accessing the Customers page.
+-- It is used to populate the table to see all entries in the Customers table
+SELECT * 
+FROM Customers;
 
--- INSERT a new customer
-INSERT INTO Customers (name, username, email) VALUES
-(:nameInput, :usernameInput, :emailInput);
+-- This query is used within the ADD CUSTOMER form. It stores the json form data in the
+-- varaible 'data' and injects the data in the query.
+INSERT INTO Customers(name, username, email) 
+VALUES ('${data['name']}', '${data['username']}', '${data['email']}');
 
--- UPDATE a customer's information
-UPDATE Customers
-SET name = :nameInput, username = :usernameInput, email = :emailInput
-WHERE customerID = :customerIDInput;
+-- This query is used within the UPDATE CUSTOMER form. It stores the json form data in the
+-- variable 'data' and injects the data in the query.
+UPDATE Customers 
+SET name='${data['name']}', username='${data['username']}', email='${data['email']}' 
+WHERE customerID = ${data['customerID']};
 
--- DELETE a customer
-DELETE FROM Customers WHERE customerID=:customerIDInput;
+-- This query is used within the DELETE CUSTOMER form. It stores the json form data in the
+-- variable 'data' and injects the data in the query
+DELETE FROM Customers 
+WHERE customerID = ${data['customerID']};
 
+-- #####################################################################
 -- ===== Orders =====
+-- #####################################################################
 
--- SELECT all order information
-SELECT * FROM Orders;
+-- This query is used when accessing the Orders page.
+-- It gets all Orders and renames the FKs column to more readable names. It then joins Customers and Games
+-- so the FKs are presented as names and titles instead of FK numbers.
+SELECT Orders.orderID, Customers.name AS customer, Games.title AS game, Orders.orderDate, Orders.pricePaid 
+FROM Orders 
+LEFT JOIN Customers ON Orders.customerID=Customers.customerID 
+LEFT JOIN Games ON Orders.gameID=Games.gameID;
+-- This query is used when accessing the orders page.
+-- It gets all the Customers data to use within the select field in the ADD ORDERS form
+SELECT * 
+FROM Customers;
+-- This query is used when accessing the orders page.
+-- It gets all the Games data to use within the select field in the ADD ORDERS form
+SELECT *
+FROM Games;
 
--- Get all orders with game names and customer names grouped together by customer name
-SELECT Customers.name, Games.title, Orders.orderDate, Orders.pricePaid
-FROM Orders
-INNER JOIN Customers ON Customers.customerID = Orders.customerID
-INNER JOIN Games ON Games.gameID = Orders.gameID
-GROUP BY Customers.name
-ORDER BY Customers.name DESC;
+-- This query is used within the ADD ORDER form. It stores the json form data in the
+-- variable 'data' and injects the data in the query
+INSERT INTO Orders(customerID, gameID, orderDate, pricePaid) 
+VALUES ('${data['customerID']}', '${data['gameID']}', '${data['orderDate']}', ${data['pricePaid']});
 
--- INSERT/Place a new order
-INSERT INTO Orders (customerID, gameID, orderDate, pricePaid) VALUES
-(:customerIDInput, :gameIDInput, GETDATE(), :pricePaidInput);
+-- This query is used within the DELETE ORDER form. It stores the json data in the
+-- variable 'data' and injects the data in the query.
+DELETE FROM Orders 
+WHERE orderID = ${data['orderID']}
 
-
+-- #####################################################################
 -- ===== Studios =====
+-- #####################################################################
 
--- SELECT all studio information
-SELECT * FROM Studios;
+-- This query is used when accessing the Studios page.
+-- It gets all the studios to display them within the table.
+SELECT * 
+FROM Studios;
 
--- INSERT/Create a new game studio
-INSERT INTO Studios (name) VALUES
-(:nameInput);
+-- This query is used within the ADD STUDIO form. It stores the json form data in the
+-- variable 'data' and injects the data in the query
+INSERT INTO Studios(name) 
+VALUES ('${data['name']}');
 
+-- This query is used within the DELETE STUDIO form. It stores the json data in the
+-- variable 'data' and injects the data in the query.
+DELETE FROM Studios 
+WHERE studioID = ${data['studioID']};
+
+-- #####################################################################
 -- ===== Games =====
+-- #####################################################################
 
--- SELECT all game information
-SELECT * FROM Games;
+-- This query is used when accessing the Games page.
+-- It gets all Games and renames the FKs column to more readable names. It then joins Studios
+-- so the FKs are presented as names instead of FK numbers.
+SELECT Games.gameID, Studios.name AS studio, Games.title, Games.publishDate, Games.price 
+FROM Games 
+INNER JOIN Studios ON Games.studioID=Studios.studioID;
+-- This query is used when accessing the Games page.
+-- It gets all the Studios data to use within the select field in the ADD GAME form
+SELECT name, studioID 
+FROM Studios;
 
--- INSERT/Add a new game
-INSERT INTO Games (studioID, title, publishDate, price) VALUES
-(:studioIDInput, :titleInput, :publishDateInput, :priceInput);
+-- This query is used within the ADD GAME form. It stores the json form data in the
+-- variable 'data' and injects the data in the query
+INSERT INTO Games(studioID, title, publishDate, price) 
+VALUES ('${data['studioID']}', '${data['title']}', '${data['publishDate']}', '${data['price']}');
 
--- SEARCH for a game based on title
-SELECT * FROM Games
-WHERE CONTAINS(title, :titleInput);
+-- This query is used within the DELETE GAME form. It stores the json data in the
+-- variable 'data' and injects the data in the query.
+DELETE FROM Games 
+WHERE Games.gameID = ${data['gameID']};
 
-
+-- #####################################################################
 -- ===== Games Genres =====
+-- #####################################################################
 
+-- This query is used when accessing the Games Genres page.
+-- It gets all Games_Genre relationships and renames the FKs column to more readable names. It then joins Genres and Games
+-- so the FKs are presented as names and titles instead of FK numbers.
+SELECT Games.title AS game, Genres.name AS genre, Games.gameID, Genres.genreID 
+FROM Games_Genres 
+INNER JOIN Games ON Games_Genres.gameID=Games.gameID 
+INNER JOIN Genres ON Games_Genres.genreID=Genres.genreID 
+ORDER BY Games.title ASC;
+-- This query is used when accessing the Games Genres page.
+-- It gets all the Games data to use within the select field in the ADD GAMES_GENRES form
+SELECT Games.gameID, Games.title 
+FROM Games;
+-- This query is used when accessing the Games Genres page.
+-- It gets all the Genres data to use within the select field in the ADD GAMES_GENRES form
+SELECT Genres.genreID, Genres.name 
+FROM Genres;
 
+-- This query is used within the ADD GAMES_GENRES form. It stores the json form data in the
+-- variable 'data' and injects the data in the query
+INSERT INTO Games_Genres(gameID, genreID) 
+VALUES ('${data['gameID']}', '${data['genreID']}');
+
+-- This query is used within the DELETE GAMES_GENRES form. It stores the json data in the
+-- variable 'data' and injects the data in the query.
+DELETE FROM Games_Genres 
+WHERE Games_Genres.genreID = ${genreID} AND Games_Genres.gameID = ${gameID};
+
+-- #####################################################################
 -- ===== Genres =====
+-- #####################################################################
 
--- SELECT/List all genres
-SELECT * From Genres;
+-- This query is used when accessing the Studios page.
+-- It gets all the genres to display them within the table.
+SELECT * 
+FROM Genres;
 
--- INSERT a new genre
-INSERT INTO Genres (name) VALUES
-(:nameInput);
+-- This query is used within the ADD GENRE form. It stores the json form data in the
+-- variable 'data' and injects the data in the query
+INSERT INTO Genres(name) 
+VALUES ('${data['name']}');
 
+-- This query is used within the DELETE GENRE form. It stores the json data in the
+-- variable 'data' and injects the data in the query.
+DELETE FROM Genres 
+WHERE Genres.genreID = ${data['genreID']};
